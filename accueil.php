38e16bf1907ca427aecb0682fab5101b193eb6ca
MarvinCOUSIN <marvin.cousin@live.fr>
2020-02-24 10:16:26 +0100

    modify accueil.php

--- accueil.php
+++ accueil.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-  <!-- Theme Made By www.w3schools.com -->
-  <title>Bootstrap Theme Company Page</title>
+  <title>SAMM</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bootstrap-3.3.7/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="bootstrap-3.3.7/js/jquery.min.js"></script>
+  <script src="bootstrap-3.3.7/js/bootstrap.min.js"></script>
   <style>
   body {
     font: 400 15px Lato, sans-serif;
