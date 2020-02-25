879a3f2a482afd9de9a9c7328be682839e2e0b03
aymanagh <47530849+aymanagh@users.noreply.github.com>
2020-02-25 15:40:58 +0100

    slt

--- bootstrap-3.3.7/js/script.js
+++ bootstrap-3.3.7/js/script.js
@@ -32,6 +32,19 @@ $(document).ready(function() {
             } // end if 
         }); // end each
     }); // end scroll
+
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+            $(".flip-card-back").hide();
+            $(".flip-card").height($('.img-responsive').height());
+        } else {
+            $(".flip-card-back").show();
+        }
+    }
+
+    var x = window.matchMedia("(max-width: 1370px)");
+    myFunction(x); // Call listener function at run time
+    x.addListener(myFunction); // Attach listener function on state changes 
 });
 
 //variable (text anime)
